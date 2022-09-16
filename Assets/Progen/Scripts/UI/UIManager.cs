4ee3dae5e5eacd02ce2eefc8d23ae89959d9030a
Niryan <niryanvalia93@gmail.com>
2022-09-16 19:54:26 +1000

    Procedural Building

---
+++ Assets/Progen/Scripts/UI/UIManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private ProGen proGen;
+
+    [SerializeField]
+    private Slider rowsSlider;
+
+    [SerializeField]
+    private Slider columnsSlider;
+    
+    [SerializeField]
+    private Slider cellUnitSlider;
+
+    [SerializeField]
+    private Slider floorsSlider;
+
+    [SerializeField]
+    private Toggle includeRoofToggle;
+
+    [SerializeField]
+    private Toggle cornersToggle;
+
+    [SerializeField]
+    private Toggle randomRowsToggle;
+
+    [SerializeField]
+    private Toggle randomColsToggle;
+
+
+    void Awake() 
+    {
+        rowsSlider.value = proGen.Theme.rows;
+        columnsSlider.value = proGen.Theme.columns;
+        cellUnitSlider.value = proGen.Theme.cellUnitSize;
+        floorsSlider.value = proGen.Theme.numberOfFloors;
+
+        includeRoofToggle.isOn = proGen.Theme.includeRoof;
+        cornersToggle.isOn = proGen.Theme.allowCornerWalls;
+        randomRowsToggle.isOn = proGen.Theme.randomizeRows;
+        randomColsToggle.isOn = proGen.Theme.randomizeColumns;
+
+    }   
+
+    void Start()
+    {
+        rowsSlider.onValueChanged.AddListener((v) => 
+        {
+            proGen.Theme.rows = (int)v;
+            proGen.Generate();
+        });
+
+        columnsSlider.onValueChanged.AddListener((v) => 
+        {
+            proGen.Theme.columns = (int)v;
+            proGen.Generate();
+        });
+
+        cellUnitSlider.onValueChanged.AddListener((v) => 
+        {
+            proGen.Theme.cellUnitSize = (int)v;
+            proGen.Generate();
+        });
+
+        floorsSlider.onValueChanged.AddListener((v) => 
+        {
+            proGen.Theme.numberOfFloors = (int)v;
+            proGen.Generate();
+        });
+
+        cornersToggle.onValueChanged.AddListener((v) => 
+        {
+            proGen.Theme.allowCornerWalls = v;
+            proGen.Generate();
+        });
+
+        randomRowsToggle.onValueChanged.AddListener((v) => 
+        {
+            proGen.Theme.randomizeRows = v;
+            proGen.Generate();
+        });
+
+        randomColsToggle.onValueChanged.AddListener((v) => 
+        {
+            proGen.Theme.randomizeColumns = v;
+            proGen.Generate();
+        });
+
+        includeRoofToggle.onValueChanged.AddListener((v) => 
+        {
+            proGen.Theme.includeRoof = v;
+            proGen.Generate();
+        });
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# ProceduralBuilding
+# Unity Builder Generator
+
+This repository contains a variety of examples created while making a new video series in YouTube about procedural generation of buildings. As a way to give back please subscribe to my [YouTube](https://www.youtube.com/c/dilmervalecillos) channel thank you !
+
+<img src="https://github.com/dilmerv/UnityBuilderGenerator/blob/master/docs/images/demo.gif" width="300">
+
+# Requirements
+* Unity 2019.2.15f1 or greater
+* Unity PostProcessing Version 2.1.7 or Greater (Manifest Requirement shown below)
+"com.unity.postprocessing": "2.1.7"
+
+For additional documentation review the PDF located at: /Assets/Progen/ProGenDocumentation.pdf
+
+This project includes 8 different scenes to help you get started with ProGen. The demo scenes are listed below and each scene name describes its purpose.
+
+* BuildingRandomizer
+<img src="https://github.com/dilmerv/UnityBuilderGenerator/blob/master/docs/images/BuildingRandomizer.png" width="300">
+
+* BuildingWithTreesCorners
+<img src="https://github.com/dilmerv/UnityBuilderGenerator/blob/master/docs/images/BuildingWithTreesCorners.png" width="300">
+
+* DarkDayBuilding
+<img src="https://github.com/dilmerv/UnityBuilderGenerator/blob/master/docs/images/DarkDayBuilding.png" width="300">
+
+* FocusOnRoofBuilding
+<img src="https://github.com/dilmerv/UnityBuilderGenerator/blob/master/docs/images/FocusOnRoofBuilding.png" width="300">
+
+* RoundedCorners
+<img src="https://github.com/dilmerv/UnityBuilderGenerator/blob/master/docs/images/RoundedCorners.png" width="300">
+
+* TwoBuildingsSimple
+<img src="https://github.com/dilmerv/UnityBuilderGenerator/blob/master/docs/images/TwoBuildingsSimple.png" width="300">
+
+* TwoBuildingsViewFromAbove
+<img src="https://github.com/dilmerv/UnityBuilderGenerator/blob/master/docs/images/TwoBuildingsViewFromAbove.png" width="300">
+
+* TwoBuildingsWithBalconies
+<img src="https://github.com/dilmerv/UnityBuilderGenerator/blob/master/docs/images/TwoBuildingsWithBalconies.png" width="300">
